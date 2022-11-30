0a90a248cb48f27c11123ad8ef6b421f7d453fa0
spacemonkey1101 <ritambh54@gmail.com>
2022-12-01 00:02:21 +0530

    component challenge

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+function Icon() {
+  return <span>🔥</span>;
+}
+
+function Card(props) {
+  return (
+    <div>
+      <h2> {props.icon} Title</h2>
+      {props.children}
+    </div>
+  );
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Card icon={<Icon />}>
+        <p>The body of the card</p>
+      </Card>
+    </>
   );
 }
 

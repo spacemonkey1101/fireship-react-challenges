45f084b9c2747a8e0acd4de2102f421e36fdb4af
spacemonkey1101 <ritambh54@gmail.com>
2023-01-09 16:32:36 +0530

    conditional rendering challenge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 
-function Icon() {
-  return <span>🔥</span>;
-}
-
-function Card(props) {
+function LoadingButton({ state, onClick, label }) {
   return (
-    <div>
-      <h2> {props.icon} Title</h2>
-      {props.children}
-    </div>
+    <button onClick={onClick} type="button">
+      {state ? <div className="loader" /> : label}{" "}
+    </button>
   );
 }
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <>
-      <Card icon={<Icon />}>
-        <p>The body of the card</p>
-      </Card>
+      <LoadingButton
+        state={isLoading}
+        onClick={() => setIsLoading(!isLoading)}
+        label="Press"
+      ></LoadingButton>
     </>
   );
 }

8d883146065112b9633c2748be72f95b6d87b3c1
spacemonkey1101 <ritambh54@gmail.com>
2023-01-15 18:28:32 +0530

    state challenge

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { useState } from "react";
 
 function App() {
-  const [state, setState] = useState({ count: 5, user: "Eve" });
+  const [currCount, setCurrCount] = useState(0);
+  const [prevCount, setPrevCount] = useState(0);
 
   const clickHandler = () => {
-    setState({
-      ...state,
-      count : state.count + 1,
-    });
-    };
+    setCurrCount(currCount +1)
+    if (currCount > 0 )
+      setPrevCount(prevCount +1)
+  };
   return (
     <>
-      <p>{state.count}</p>
+      <h1>Current Count : {currCount}</h1>
+      <h1>Previous Count : {prevCount}</h1>
+
       <button onClick={clickHandler}>Increase</button>
     </>
   );

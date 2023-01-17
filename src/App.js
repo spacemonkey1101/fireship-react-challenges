ec272f56c1c57b841a91aa735c99bb005200eeb0
spacemonkey1101 <ritambh54@gmail.com>
2023-01-17 10:00:44 +0530

    lifecycle with useEffect

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [currCount, setCurrCount] = useState(0);
-  const [prevCount, setPrevCount] = useState(0);
+  const [count] = useState(0);
 
-  const clickHandler = () => {
-    setCurrCount(currCount +1)
-    if (currCount > 0 )
-      setPrevCount(prevCount +1)
-  };
-  return (
-    <>
-      <h1>Current Count : {currCount}</h1>
-      <h1>Previous Count : {prevCount}</h1>
+  useEffect(() => {
+    console.log("count updated")
+    return () =>  console.log("destroyed!")
+  },[count]);
 
-      <button onClick={clickHandler}>Increase</button>
-    </>
-  );
 }
 
 export default App;
